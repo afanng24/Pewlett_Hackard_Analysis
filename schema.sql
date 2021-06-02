05002208ac24863bcb8e9c164d6ab251f7ca046e
Alan Luo Fanng <afanng24@gmail.com>
2021-06-02 01:07:21 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -36,19 +36,35 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title varchar NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+    emp_no int  NOT NULL ,
+    title varchar  NOT NULL ,
+    from_date date  NOT NULL ,
+    to_date date  NOT NULL 
 );
 
 
-
-
-Select * from departments;
+Select * from employees;
 
 SELECT first_name, last_name
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+Select * From titles;
+
